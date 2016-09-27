018fc9ced57997838e2c8e3a7098eb118fc5773e
Roman Sukhorukov <roman.a.sukhorukov@gmail.com>
2016-09-27 13:47:13 +0300

    Inject babel-plugin-external-helpers to babel-loader queryParams.

--- src/webpack-configuration.js
+++ src/webpack-configuration.js
@@ -1,3 +1,4 @@
+const qs = require('qs');
 const {isObject, ensureArray} = require('./helpers');
 
 module.exports = function modifyWebpackConfiguration(compiler, pluginOptions) {
@@ -14,7 +15,7 @@ module.exports = function modifyWebpackConfiguration(compiler, pluginOptions) {
 function includeExternalHelpersPlugin(configuration, pluginOptions) {
     let aliases = getBabelLoaderAliases(pluginOptions);
     let loadersParams = getBabelLoaders(configuration, aliases);
-    loadersParams.forEach(loaderParams => addQueryParam(loaderParams, aliases));
+    loadersParams.forEach(loaderParams => addQueryParams(loaderParams, aliases));
 
     return loadersParams.length > 0;
 }
@@ -24,8 +25,7 @@ function getBabelLoaderAliases(pluginOptions) {
 }
 
 function getBabelLoaders(configuration, aliases) {
-    return getLoaders(configuration)
-        .filter(loaderParams => matchLoaderByAlias(loaderParams, aliases));
+    return getLoaders(configuration).filter(loaderParams => matchLoaders(loaderParams, aliases));
 }
 
 function getLoaders(configuration) {
@@ -34,28 +34,18 @@ function getLoaders(configuration) {
     return Array.isArray(loaders) ? loaders : [];
 }
 
-function matchLoaderByAlias(loaderParams, aliases) {
-    return extractLoaderNames(loaderParams).some(loaderName => {
-        loaderName = loaderName.toLowerCase();
-
-        return aliases.some(alias => {
-            if (alias instanceof RegExp) {
-                return alias.test(loaderName);
-            }
-
-            return alias.toLowerCase() === loaderName;
-        });
-    });
-}
-
-function extractLoaderNames(loaderParams) {
+function matchLoaders(loaderParams, aliases) {
     let loaders = Array.isArray(loaderParams.loaders) ? loaderParams.loaders : [];
     if (loaderParams.loader && typeof loaderParams.loader === 'string') {
         loaders = loaderParams.loader.split('!');
     }
 
-    return loaders.reduce((result, loaderString) => {
-        let loaderName = loaderString && loaderString.split('?')[0];
+    return extractLoaderNames(loaders).some(loaderName => matchLoaderNameByAliases(loaderName, aliases));
+}
+
+function extractLoaderNames(loaderStrings) {
+    return loaderStrings.reduce((result, loaderString) => {
+        let loaderName = extractLoaderName(loaderString);
         if (loaderName) {
             result.push(loaderName);
         }
@@ -64,33 +54,94 @@ function extractLoaderNames(loaderParams) {
     }, []);
 }
 
-// TODO: implement
-function addQueryParam(loaderParams, aliases) {
+function extractLoaderName(loaderString = '') {
+    return loaderString.split('?')[0];
+}
+
+function extractLoaderQuery(loaderString = '') {
+    const querySignIndex = loaderString.indexOf('?');
+    let result = '';
+    if (querySignIndex > -1) {
+        result = loaderString.substring(querySignIndex + 1);
+    }
+
+    return result;
+}
+
+function matchLoaderNameByAliases(loaderName, aliases) {
+    loaderName = loaderName.toLowerCase();
+
+    return aliases.some(alias => {
+        if (alias instanceof RegExp) {
+            return alias.test(loaderName);
+        }
+
+        return alias.toLowerCase() === loaderName;
+    });
+}
+
+function addQueryParams(loaderParams, aliases) {
     if (loaderParams.query) {
-        addQueryParamToLoaderQuery(loaderParams);
+        loaderParams.query = injectQueryParamsToLoaderQuery(loaderParams.query);
 
         return;
     }
 
-    if (typeof loaderParams.loader === 'string') {
-        loaderParams.loader = addQueryParamToLoaderString(loaderParams.loader, aliases);
+    let loaderString = (typeof loaderParams.loader === 'string') ? loaderParams.loader : null;
+    if (loaderString) {
+        loaderParams.loader = injectQueryParamsToLoaderString(loaderString, aliases);
     } else if (Array.isArray(loaderParams.loaders)) {
-        addQueryParamToLoadersArray(loaderParams, aliases);
+        loaderParams.loaders = injectQueryParamsToLoadersArray(loaderParams.loaders, aliases);
     }
 }
 
-// TODO: implement
-function addQueryParamToLoaderString(loaderString, aliases) {
-    return loaderString;
+function injectQueryParamsToLoaderQuery(query) {
+    const queryObject = getQueryObject(query);
+    injectPluginsParams(queryObject);
+
+    return qs.stringify(queryObject, {arrayFormat: 'brackets', encode: false});
 }
 
-// TODO: implement
-function addQueryParamToLoadersArray(loaderParams, aliases) {
-    return loaderParams;
+function getQueryObject(query) {
+    if (typeof query === 'string') {
+        query = parseQueryString(query);
+    }
+
+    return query;
 }
 
-// TODO: implement
-function addQueryParamToLoaderQuery(loaderParams) {
+function parseQueryString(queryString) {
+    if (queryString.charAt(0) === '{' && queryString.charAt(queryString.length - 1) === '}') {
+        return JSON.parse(queryString);
+    }
+
+    return qs.parse(queryString, {allowDots: true});
+}
+
+function injectPluginsParams(query) {
+    const helpersPluginRegexp = /(?:^|babel-plugin-)external-helpers$/i;
+    const plugins = Array.isArray(query.plugins) ? query.plugins : [];
+    if (!plugins.some(pluginName => helpersPluginRegexp.test(pluginName))) {
+        plugins.push('external-helpers');
+    }
+    query.plugins = plugins.filter(Boolean);
+}
+
+function injectQueryParamsToLoaderString(loaders, aliases) {
+    return injectQueryParamsToLoadersArray(loaders.split('!'), aliases).join('!');
+}
+
+function injectQueryParamsToLoadersArray(loaderStrings, aliases) {
+    return loaderStrings.map(loaderString => {
+        let loaderName = extractLoaderName(loaderString);
+        if (!matchLoaderNameByAliases(loaderName, aliases)) {
+            return loaderString;
+        }
+
+        let queryString = injectQueryParamsToLoaderQuery(extractLoaderQuery(loaderString));
+
+        return `${loaderName}?${queryString}`;
+    });
 }
 
 function fixConfigurationEntry(configuration, onlyEntries) {
