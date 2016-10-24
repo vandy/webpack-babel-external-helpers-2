034b2f859c8777a502d76757c3b6e3ee1cdfd08e
Roman Sukhorukov <roman.a.sukhorukov@gmail.com>
2016-10-24 10:44:10 +0300

    Added new method "has" to helpers.

--- src/helpers.js
+++ src/helpers.js
@@ -9,3 +9,11 @@ module.exports.isStringOrArray = function (value) {
 module.exports.ensureArray = function (value) {
     return Array.isArray(value) ? value : [value];
 };
+
+exports.has = (function () {
+    const has = Object.prototype.hasOwnProperty;
+
+    return function (obj, key) {
+        return has.call(obj, key);
+    };
+}());
